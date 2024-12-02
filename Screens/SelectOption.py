403ba12d410fb3209cebb33e1ca291c15daca11a
LysM0rke99 <163895619+AlexiosCrane99@users.noreply.github.com>
2024-12-02 08:07:23 -0500

    Arreglo imagen, energy y water

--- Screens/SelectOption.py
+++ Screens/SelectOption.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-from PIL import Image, ImageTk
 import os
 import sys
 import importlib.util
@@ -22,25 +21,17 @@ def ChangeScreen(path):
 # Crear la nueva ventana como Toplevel
 nueva_ventana = tk.Toplevel()
 nueva_ventana.title("Options")
-nueva_ventana.geometry("500x500")
+nueva_ventana.geometry("650x770")
 nueva_ventana.configure(bg="#3B8C6E")
 
+imagen = tk.PhotoImage(file="Assets/oit17.png")
+
+# Crear un widget Label y asignarle la imagen
+etiqueta = tk.Label(nueva_ventana, image=imagen)
+etiqueta.pack()
 # Título
 titulo = ttk.Label(nueva_ventana, text="Selecciona una Opción", font=("Arial", 16, "bold"), background="#3B8C6E", foreground="white")
 titulo.pack(pady=20)
-
-# Redimensionar la imagen usando Pillow
-try:
-    imagen_original = Image.open("Assets/oit17.png")
-    imagen_redimensionada = imagen_original.resize((300, 300))
-    imagen = ImageTk.PhotoImage(imagen_redimensionada)  # Crear referencia global
-
-    etiqueta_imagen = ttk.Label(nueva_ventana)
-    etiqueta_imagen.image = imagen  # Guardar referencia
-    etiqueta_imagen.config(image=imagen)
-    etiqueta_imagen.pack(pady=10)
-except FileNotFoundError:
-    print("Error: La imagen 'oit17.png' no se encuentra en la carpeta 'Assets'.")
 
 # Frame para los botones
 botones_frame = tk.Frame(nueva_ventana, bg="#3B8C6E")
